154885e51f7500ad398934eea98e465d8ddfc193
杨伟诚 <helloywc@qq.com>
2020-09-08 19:31:56 +0800

    加入图标页

--- src/views/develop/icons/icons.vue
+++ src/views/develop/icons/icons.vue
@@ -1,18 +1,114 @@
 <template>
   <div class="app-container">
     <h1>icons</h1>
+    <div class="svg-icon-library">
+      <div class="title">
+        <div>name</div>
+        <div>16*16</div>
+        <div>32*32</div>
+        <div>48*48</div>
+      </div>
+      <!--  -->
+      <div class="icon-list">
+        <div v-for="item in iconLibrary"
+             :key="item"
+             class="row">
+          <!-- 名称 -->
+          <div class="name">{{item}}</div>
+          <!--  -->
+          <div class="icon">
+            <svg-icon :icon-class="item" />
+          </div>
+          <!--  -->
+          <div class="icon">
+            <svg-icon :icon-class="item"
+                      class="icon svg-icon-background" />
+          </div>
+          <!--  -->
+          <div class="icon">
+            <svg-icon :icon-class="item"
+                      class="icon svg-icon-fill" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default{
   name: 'userDetail',
+  data() {
+    return {
+      // 图标库
+      iconLibrary: [],
+    };
+  },
   methods: {
-
+    getIconLibrary() {
+      this.iconLibrary = require
+        .context('@/icons/svg', false, /\.svg$/)
+        .keys()
+        .map(item => item.replace(/^(\.\/)|(\.svg)$/g, ''));
+    },
   },
   created(){
-    console.log(123)
+    this.getIconLibrary()
   },
+
+
+
 }
 </script>
+
+<style lang="scss" scoped>
+  .svg-icon-library {
+    .title {
+      width: 300px;
+      display: flex;
+      font-size: 16px;
+      font-weight: bold;
+      line-height: 30px;
+      div {
+        width: 50px;
+        text-align: center;
+        &:first-child {
+          width: 120px;
+        }
+      }
+    }
+    .icon-list {
+      display: flex;
+      flex-wrap: wrap;
+      .row {
+        height: 50px;
+        width: 300px;
+        display: flex;
+        align-items: center;
+        border-right: 1px solid #30b08f;
+        .name {
+          width: 120px;
+          font-size: 16px;
+          text-align: center;
+        }
+        .icon {
+          height: 50px;
+          width: 50px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .svg-icon-background {
+          height: 32px;
+          width: 32px;
+          background: #30b08f;
+        }
+        .svg-icon-fill {
+          height: 48px;
+          width: 48px;
+          fill: #30b08f;
+        }
+      }
+    }
+  }
+</style>
